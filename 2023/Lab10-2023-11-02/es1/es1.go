ea8e8f53e53d9ca0393246e20890279b00e0b0d4
MartinWWDC <60852345+MartinWWDC@users.noreply.github.com>
2023-11-12 17:03:51 +0100

    es 1.2

--- 2023/Lab10-2023-11-02/es1/es1.go
+++ 2023/Lab10-2023-11-02/es1/es1.go
@@ -79,14 +79,50 @@ func isOperatore(token string)bool{
 }
 
 // 1.2. Conversione da notazione infissa a notazione postfissa
+func converti(espressione string)string{
+	str:=""
+	var s Stack
+	pila := newStack()
+	s = &pila
 
+	var tokens []string = strings.Split(espressione, " ")
+	//leggi un token ;
+	for _, token := range tokens {
+		n,err:= strconv.Atoi(token)
+		//se il token è un numero
+		if err==nil{
+			//stampa il token ;
+			fmt.Println(n)
+			str+=strconv.Itoa(n)
+		}
+		//se il token è un operatore
+		if isOperatore(token){
+			//inserisci il token in cima alla pila ;
+			s.Push(token)
+			
+		}
+		//se il token è una parentesi aperta
+		//ignora il token
+		if token==")"{
+			//se il token è una parentesi chiusa
+			//estrai l ’ operatore in cima alla pila ;
+			str+=s.Pop().(string)
+			//stampalo ;
+			
+		}
+		
+		
+	}
+	return str
+}
 
 
 func main() {
 	// 1.1.
-	var espressione1 string = "5 3 - 2 *"
-	fmt.Println(valuta(espressione1))
+	//var espressione1 string = "5 3 - 2 *"
+	var espressione2 string = "( ( 5 - 3 ) * 2 )"
+	//fmt.Println(valuta(espressione1))
 
 	// 1.2
-	// Puppatina al bomberazzo??	
+	fmt.Println(converti(espressione2))
 }

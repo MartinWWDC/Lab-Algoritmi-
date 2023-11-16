f45a2363e9bd9ec01a3525ff6bdd6c7c28715c3c
MartinWWDC <60852345+MartinWWDC@users.noreply.github.com>
2023-11-16 12:57:30 +0100

    init test

--- 2023/Lab13-2023-11-13/es1/es1.go
+++ 2023/Lab13-2023-11-13/es1/es1.go
@@ -3,5 +3,9 @@ package main
 func main() {
 
 	sonsMap := make(map[string][]string)
+	sonsMap["Anna"] = []string{"Bruno", "Carlo", "Daniela"}
+	sonsMap["Bruno"] = []string{"Enrico", "Francesco"}
+	sonsMap["Gianni"] = []string{"Henry"}
+	sonsMap["Francesco"] = []string{"Irene"}
 
 }

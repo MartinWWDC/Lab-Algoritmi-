a65b0f0b7f60c873d902def9dc10aa02fd62147e
MartinWWDC <60852345+MartinWWDC@users.noreply.github.com>
2023-11-16 13:12:24 +0100

    es1: quantiSenzaSubordinati

--- 2023/Lab13-2023-11-13/es1/es1.go
+++ 2023/Lab13-2023-11-13/es1/es1.go
@@ -11,9 +11,25 @@ func main() {
 	sonsMap["Francesco"] = []string{"Irene"}
 
 	stampaSbordinati(sonsMap,"Anna")
+	c:=quantiSenzaSubordinati(sonsMap)
+	fmt.Println(c)
 
 }
 
 func stampaSbordinati(sonsMap map[string][]string, key string) {
 	fmt.Println(sonsMap[key])
+}
+
+func quantiSenzaSubordinati(sonsMap map[string][]string)int{
+	counter:=0
+	for _,arr:= range sonsMap{
+		for _,el:=range arr{
+			_,check:=sonsMap[el]
+			if !check{
+				//fmt.Println(el)
+				counter++
+			}
+		}				
+	}
+	return counter
 }
